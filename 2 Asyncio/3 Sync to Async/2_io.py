692874275242bee624df9666aeddea45b1738e13
Madi-S <shaiken.m@mail.ru>
2024-01-23 22:37:16 +0600

    Sync to Async examples added

---
+++ 2 Asyncio/3 Sync to Async/2_io.py
@@ -0,0 +1,34 @@
+import asyncio
+import datetime
+import requests
+import concurrent.futures
+
+
+def blocking_task():
+    requests.get('https://docs.python.org/3')
+
+
+async def blocking_worker():
+    while True:
+        blocking_task()
+
+
+async def async_thread_worker():
+    # converting sync operation to async
+    loop = asyncio.get_event_loop()
+    with concurrent.futures.ThreadPoolExecutor(2) as pool:
+        while True:
+            await loop.run_in_executor(pool, blocking_task)
+
+
+async def ticker():
+    while True:
+        print(datetime.datetime.now())
+        await asyncio.sleep(1)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.create_task(ticker())
+    loop.create_task(async_thread_worker())
+    loop.run_forever()
