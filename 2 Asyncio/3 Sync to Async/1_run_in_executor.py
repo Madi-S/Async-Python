ef0fcfc4344e2d68e5eb1a2507645a1d11378217
Madi-S <shaiken.m@mail.ru>
2024-01-24 17:11:55 +0600

    Formatting

--- 2 Asyncio/3 Sync to Async/1_run_in_executor.py
+++ 2 Asyncio/3 Sync to Async/1_run_in_executor.py
@@ -21,18 +21,18 @@ async def main():
     # Options:
 
     # 1. Run in the	default	loop's executor:
-    result = await loop.run_in_executor(None,	blocking_io)
-    print('default	thread	pool',	result)
+    result = await loop.run_in_executor(None, blocking_io)
+    print('default thread pool', result)
 
     # 2. Run in a custom thread pool:
     with concurrent.futures.ThreadPoolExecutor() as pool:
-        result = await loop.run_in_executor(pool,	blocking_io)
-        print('custom	thread	pool',	result)
+        result = await loop.run_in_executor(pool, blocking_io)
+        print('custom thread pool', result)
 
     # 3. Run in a custom process pool:
     with concurrent.futures.ProcessPoolExecutor() as pool:
-        result = await loop.run_in_executor(pool,	cpu_bound)
-        print('custom	process	pool',	result)
+        result = await loop.run_in_executor(pool, cpu_bound)
+        print('custom process pool', result)
 
 
 if __name__ == '__main__':
