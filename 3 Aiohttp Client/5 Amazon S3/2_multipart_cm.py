9325838125cc3a019656525f38ff47b90b567103
Madi-S <shaiken.m@mail.ru>
2024-02-15 10:44:54 +0600

    MultipartUpload explained

---
+++ 3 Aiohttp Client/5 Amazon S3/2_multipart_cm.py
@@ -0,0 +1,55 @@
+
+
+class MultipartUploader:
+    def __init__(self, client, bucket: str, key: str):
+        self.client = client
+        self.bucket = bucket
+        self.key = key
+        
+        self.part_number: int = 0
+        self.parts: list[dict] = []
+        self.mpu: dict | None = None
+        self.uploaded_size: float = 0
+        self.is_loading: bool = False
+        
+    async def __aenter__(self):
+        await self._create_uploading()
+        return self
+    
+    async def __aexit__(self, exc_type, exc_value, exc_tb):
+        if not exc_type:
+            await self._finish_uploading()
+        else:
+            await self.client.abort_multipart_upload(
+                Bucket=self.bucket,
+                Key=self.key,
+                Uploadid=self.mpu['UploadId']
+            )
+    
+    async def _create_uploading(self) -> None:
+        self.parts = []
+        self.part_number = 1
+        
+        self.mpu = await self.client.create_multipart_upload(
+            BUcket=self.bucket,
+            Key=self.key
+        )
+        
+        self.is_loading = True
+        self.uploaded_size = 0
+        
+        
+    async def upload_part(self, chunk: bytes) -> None:
+        part = await self.client.upload_part(
+            Bucket=self.bucket,
+            Key=self.key,
+            PartNumber=self.part_number,
+            UploadId=self.mpu['UploadId']
+            Body=chunk
+        )
+    
+        self.parts.append({'PartNumber': self.part_number, 'ETag': part['ETag']})
+        self.part_number += 1
+        
+        self.uploaded_size += len(chunk) / 1024 / 1024
+        print('Uploaded', self.uploaded_size)
