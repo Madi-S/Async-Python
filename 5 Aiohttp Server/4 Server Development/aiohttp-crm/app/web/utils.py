2463b2ff3aed4fb3f3a95bb7fc682cac2cb48eb4
Madi-S <shaiken.m@mail.ru>
2024-02-25 18:07:54 +0600

    Aiohttp Server error handling middleware added

--- 5 Aiohttp Server/4 Server Development/aiohttp-crm/app/web/utils.py
+++ 5 Aiohttp Server/4 Server Development/aiohttp-crm/app/web/utils.py
@@ -9,3 +9,21 @@ def json_response(data=None, status: str = 'ok') -> Response:
         'status': status,
         'data': data
     })
+
+
+def error_json_response(
+    http_status: int,
+    status: str = 'error',
+    message: str | None = None,
+    data: dict | None = None
+):
+    if data is None:
+        data = {}
+    return aiohttp_json_response(
+        status=http_status,
+        data={
+            'status': status,
+            'message': str(message),
+            'data': data
+        }
+    )
